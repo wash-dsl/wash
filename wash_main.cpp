2c18013f1886046aaef323aaa4e369b24d0e211d
Tom Divers <Tom.Divers@warwick.ac.uk>
2023-11-17 16:19:42 +0000

    added gravity and refactored volume (untested)

--- wash_main.cpp
+++ wash_main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm> // for std::max
 
 #include "wash_serial.hpp"
+#include "wash_vector.cpp"
 
 #define DELTA_TIME 0.1
 #define SMOOTH_RAD 20
@@ -8,6 +9,7 @@
 // TODO:
 // - Kernel function for density calculation?
 //
+
 
 double user_smoothing_derivative(double radius, double dist) {
     double value = std::max(0.0, radius * radius - dist * dist);
@@ -24,14 +26,16 @@ void force_kernel(wash::Particle &p, std::vector<wash::Particle> &neighbours) {
         double dist = wash::eucdist(p, q);
         wash::Vec2D dir = (p.get_pos() - q.get_pos()) / dist;
         double slope = user_smoothing_derivative(SMOOTH_RAD, dist);
-        pressure_force += dir * -q.get_force_scalar("pressure") * q.get_force_scalar("vol") * slope;
+        pressure_force += dir * -q.get_force_scalar("pressure") * q.get_vol() * slope;
     }
 
     p.set_force_vector("pressure", pressure_force);
 }
 
+const wash::Vec2D gravity{0, -1};
+
 void update_kernel(wash::Particle &p) {
-    p.set_acc(p.get_force_vector("pressure") / p.get_density());
+    p.set_acc( (p.get_force_vector("pressure") / p.get_density()) + gravity );
     p.set_vel(p.get_vel() + p.get_acc() * DELTA_TIME);
     p.set_pos(user_bounds_check(p.get_pos() + p.get_vel() * DELTA_TIME));
 }
@@ -56,7 +60,6 @@ int main(int argc, char **argv) {
     wash::set_max_iterations(100);
     wash::add_force("temp");
     wash::add_force("pressure");
-    wash::add_force("vol");
 
     wash::set_init_kernel(&init);
     wash::set_force_kernel(&force_kernel);
