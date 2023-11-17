08b0e36d7dd9046b9d09b36f3e82b9cfd2330d13
Scott Parker <scott.parker.uk@btinternet.com>
2023-11-17 15:38:10 +0000

    Sphinx site pushed instead of doxygen

--- docs/run.sh
+++ docs/run.sh
@@ -1,1 +1,1 @@
-doxygen Doxyfile.in && sphinx-build -b html -Dbreathe_projects.my_project=doc_out/xml . doc_out/sphinx/ && cp -a doc_out/html/. ../docs
+doxygen Doxyfile.in && sphinx-build -b html -Dbreathe_projects.my_project=doc_out/xml . doc_out/sphinx/ && cp -a doc_out/sphinx/. ../docs
