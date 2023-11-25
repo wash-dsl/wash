7e96c007dbf0bdd07f72d3add64a62943c213168
Mikołaj Wąsacz <56759408+mwasacz@users.noreply.github.com>
2023-11-25 22:58:18 +0000

    Fix formatting

--- sedov_blast_wave/sedov_init.cpp
+++ sedov_blast_wave/sedov_init.cpp
@@ -15,29 +15,26 @@ void init() {
 
     double cv = ideal_gas_cv;
 
-    for (size_t i = 0; i < num_part_1d + 1; ++i)
-    {
+    for (size_t i = 0; i < num_part_1d + 1; ++i) {
         double zpos = r_ini + (i * step);
 
-        for (size_t j = 0; j < num_part_1d; ++j)
-        {
+        for (size_t j = 0; j < num_part_1d; ++j) {
             double ypos = r_ini + (j * step);
 
-            for (size_t k = 0; k < num_part_1d; ++k)
-            {
+            for (size_t k = 0; k < num_part_1d; ++k) {
                 size_t lindex = (i * num_part_1d * num_part_1d) + (j * num_part_1d) + k;
                 double xpos = r_ini + (k * step);
 
-                    double r2 = xpos * xpos + ypos * ypos + zpos * zpos;
-                    double ui = ener0 * std::exp(-(r2 / (width * width))) + u0;
-                    
-                    double temp = ui / cv;
+                double r2 = xpos * xpos + ypos * ypos + zpos * zpos;
+                double ui = ener0 * std::exp(-(r2 / (width * width))) + u0;
 
-                    wash::Particle p({xpos, ypos, zpos}, width);
-        
-                    // p.set_force("mui", &mui);
-                    p.set_force("temp", &temp);
-                    wash::add_par(p);
+                double temp = ui / cv;
+
+                wash::Particle p({xpos, ypos, zpos}, width);
+
+                // p.set_force("mui", &mui);
+                p.set_force("temp", &temp);
+                wash::add_par(p);
             }
         }
     }
