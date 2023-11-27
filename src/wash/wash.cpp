888948f00c88096d7dffac6b7362524a2cafe255
Mikołaj Wąsacz <56759408+mwasacz@users.noreply.github.com>
2023-11-27 21:52:42 +0000

    Implement Simulation and Kernel class functions

--- src/wash/wash.cpp
+++ src/wash/wash.cpp
@@ -1,16 +1,75 @@
 #include "wash.hpp"
 
 namespace wash {
-    Particle::Particle(const int id)
-        : id(id),
-          density(0.0),
-          mass(0.0),
-          smoothing_length(0.0),
-          pos(SimulationVecT{}),
-          vel(SimulationVecT{}),
-          acc(SimulationVecT{}),
-          force_scalars(std::unordered_map<std::string, double>({})),
-          force_vectors(std::unordered_map<std::string, wash::SimulationVecT>({})) {}
+    uint64_t Simulation::get_max_iterations() const { return max_iterations; }
+
+    void Simulation::set_max_iterations(const uint64_t iterations) { this->max_iterations = iterations; }
+
+    void Simulation::add_force_scalar(const std::string force) { this->forces_scalar.push_back(force); }
+
+    void Simulation::add_force_vector(const std::string force) { this->forces_vector.push_back(force); }
+
+    void Simulation::add_variable(const std::string variable, double init_value) {
+        this->variables.emplace(variable, init_value);
+    }
+
+    void Simulation::add_init_kernel(const VoidFuncT func) { this->init_kernels.push_back(VoidKernel(func)); }
+
+    void Simulation::add_kernel(const ParticleNeighborsFuncT func) {
+        this->loop_kernels.push_back(ParticleNeighborsKernel(func));
+    }
+
+    void Simulation::add_kernel(const ParticleFuncT func) { this->loop_kernels.push_back(ParticleKernel(func)); }
+
+    void Simulation::add_kernel(const ParticleMapFuncT map_func, const ReduceFuncT reduce_func, const double seed,
+                                const std::string variable) {
+        this->loop_kernels.push_back(ParticleReduceKernel(map_func, reduce_func, seed, variable));
+    }
+
+    void Simulation::add_kernel(const VoidFuncT func) { this->loop_kernels.push_back(VoidKernel(func)); }
+
+    Particle& Simulation::create_particle(const double density, const double mass, const double smoothing_length,
+                                          const SimulationVecT pos, const SimulationVecT vel,
+                                          const SimulationVecT acc) {
+        auto id = this->particles.size();
+        auto& p = particles.emplace_back(id);
+        p.set_density(density);
+        p.set_mass(mass);
+        p.set_smoothing_length(smoothing_length);
+        p.set_pos(pos);
+        p.set_vel(vel);
+        p.set_acc(acc);
+        return p;
+    }
+
+    double Simulation::get_variable(const std::string& variable) const { return this->variables.at(variable); }
+
+    void Simulation::set_variable(const std::string& variable, const double value) {
+        this->variables.at(variable) = value;
+    }
+
+    std::vector<Particle>& Simulation::get_particles() { return this->particles; }
+
+    void Simulation::start() {
+        for (auto& k : init_kernels) {
+            k.exec(*this);
+        }
+        for (uint64_t iter = 0; iter < max_iterations; iter++) {
+            for (auto& k : loop_kernels) {
+                k.exec(*this);
+            }
+        }
+    }
+
+    void Simulation::set_simulation_name(const std::string name) { this->simulation_name = name; }
+
+    void Simulation::set_output_file_name(const std::string name) { this->output_file_name = name; }
+
+    const std::vector<std::string>& Simulation::get_forces_scalar() const { return this->forces_scalar; }
+
+    const std::vector<std::string>& Simulation::get_forces_vector() const { return this->forces_vector; }
+
+    Particle::Particle(const int id) : id(id) {}
 
     double Particle::get_density() const { return this->density; }
 
@@ -49,6 +108,44 @@ namespace wash {
     }
 
     double Particle::get_vol() const { return get_mass() / get_density(); }
+
+    ParticleNeighborsKernel::ParticleNeighborsKernel(const ParticleNeighborsFuncT func) : func(func) {}
+
+    void ParticleNeighborsKernel::exec(Simulation& sim) const {
+        for (auto& p : sim.get_particles()) {
+            std::vector<Particle> neighbors;
+            for (auto& q : sim.get_particles()) {
+                if (&p != &q) {  // TODO: check distance
+                    neighbors.push_back(q);
+                }
+            }
+            func(sim, p, neighbors);
+        }
+    }
+
+    ParticleKernel::ParticleKernel(const ParticleFuncT func) : func(func) {}
+
+    void ParticleKernel::exec(Simulation& sim) const {
+        for (auto& p : sim.get_particles()) {
+            func(sim, p);
+        }
+    }
+
+    ParticleReduceKernel::ParticleReduceKernel(const ParticleMapFuncT map_func, const ReduceFuncT reduce_func,
+                                               const double seed, const std::string variable)
+        : map_func(map_func), reduce_func(reduce_func), seed(seed), variable(variable) {}
+
+    void ParticleReduceKernel::exec(Simulation& sim) const {
+        auto result = seed;
+        for (auto& p : sim.get_particles()) {
+            result = reduce_func(result, map_func(sim, p));
+        }
+        sim.set_variable(variable, result);
+    }
+
+    VoidKernel::VoidKernel(const VoidFuncT func) : func(func) {}
+
+    void VoidKernel::exec(Simulation& sim) const { func(sim); }
 
     double eucdist(const Particle& p, const Particle& q) {
         auto pos = p.get_pos() - q.get_pos();
